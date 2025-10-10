1c683e23170928fce3bae5fb4ecef8261381c5f0
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2025-10-09 22:42:54 -0600

    Add CSS Showcase component with styles and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CreateMeds } from "./pages/create-meds/create-meds";
 import {ListMeds} from './pages/list-meds/list-meds';
 import {Locations} from './pages/locations/locations';
+import {CssShowcase} from './pages/css-showcase/css-showcase';
 
 export const routes: Routes = [
   { path: 'create-meds', component: CreateMeds, title: 'Crear Medicamentos' },
   { path: 'list-meds', component: ListMeds, title: 'Lista Medicamentos'},
   { path: 'locations', component: Locations, title: 'Ubicaciones'},
+  { path: 'css-showcase', component: CssShowcase, title: 'Consejos de Salud'},
   { path: '**', component: CreateMeds }
 ];
---
+++ src/app/pages/css-showcase/css-showcase.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css-showcase',
+  imports: [],
+  templateUrl: './css-showcase.html',
+  styleUrl: './css-showcase.css'
+})
+export class CssShowcase {
+
+}
